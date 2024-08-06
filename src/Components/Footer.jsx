9458b1fc37aecb1a52914897d4266e24aff4fb16
Muhib Sami <muhib.sami.pk@gmail.com>
2024-08-06 23:48:17 +0500

    changes footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,11 +4,11 @@ const lists = [
   {
     label: "Home",
     headings: [
-      "Brownze arnold",
-      "Chronograph blue",
-      "Smart phones",
-      "Automatic watch",
-      "Hair straighteners",
+      "Browne Arnold",
+      "Chronograph Blue",
+      "Smart Phones",
+      "Automatic Watch",
+      "Hair Straighteners",
     ],
   },
   {
@@ -16,31 +16,35 @@ const lists = [
     headings: [
       "About us",
       "Contact us",
-      "Return policy",
-      "Privacy policy",
-      "Payment policy",
+      "Return Policy",
+      "Privacy Policy",
+      "Payment Policy",
     ],
   },
   {
     label: "Help",
-    headings: ["News", "Service", "Our policy", "Custmer care", "Faq’s"],
+    headings: ["News", "Service", "Our Policy", "Custmer Care", "FAQS"],
+  },
+  {
+    label: "Team Member:",
+    headings: ["Muhib Sami", "Saqib Saleem", "Talha Shehzad"],
   },
 ];
 
 const Footer = () => {
-  const footerLine = "64 st james boulevard hoswick ,<br/> ze2 7zj";
+  const footerLine = "34th street PetersBourgh Boulevard New York ,<br/> USA Film Makers";
 
   return (
-    <div className="bg-[#14143c] text-white py-5">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-2">
-        <div className="flex flex-col justify-center items-start md:items-center px-6 md:px-2 gap-2">
+    <div className="bg-[#14143c] text-white py-8">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-7">
+        <div className="flex flex-col justify-center items-start md:items-center px-7 md:px-2 gap-2">
           <img src="./logo.png" className="h-30 w-62" alt="logo" />
           <p
             className="text-center"
             dangerouslySetInnerHTML={{ __html: footerLine }}
           />
         </div>
-        <div className="col-span-1 md:col-span-3  text-white grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-4 md:px-0">
+        <div className="col-span-1 md:col-span-3  text-white grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-6 md:px-0">
           {lists.map((list, index) => (
             <div key={index}>
               <h3 className="font-sans text-lg font-semibold text-white mb-2">
@@ -50,7 +54,7 @@ const Footer = () => {
                 {list.headings.map((subList, subListIndex) => (
                   <li
                     key={subListIndex}
-                    className="text-white text-sm font-light mb-1"
+                    className="text-white text-sm font-Dark mb-2"
                   >
                     {subList}
                   </li>
